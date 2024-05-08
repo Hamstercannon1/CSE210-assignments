e2959741593faa10318ca85f4e769fccc9c123a5
Tanner Johnson <tqjohnson17@gmail.com>
2024-05-08 12:28:16 -0600

    hi

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("Please input your grade percentage: ");
+        string percent = Console.ReadLine();
+        int numberGradeTest = int.Parse(percent);
+
+        string letter="";
+
+        if (numberGradeTest >= 90)
+        {
+            letter ="A";
+            
+            
+        }
+        else if (numberGradeTest >= 80)
+        {
+            letter="B";
+        }
+
+        else if (numberGradeTest >= 70)
+        {
+            letter = "C";
+        }
+        else if (numberGradeTest >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is {letter} ");
+
+        if (numberGradeTest>=70)
+        {
+            Console.WriteLine("Congratulations! You passed the course! ");
+        }
+        else
+        {
+            Console.WriteLine("Better luck next time. ");
+        }
     }
 }
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
+        Course course1 = new Course();
+        Console.WriteLine("Are we having fun yet? ");
+        course1._className = "Prog. w/Classes";
+        course1._color = "blue";
+        course1._courseCode = "CSE 201";
+        course1._numberOfCredits = 2;
+        course1.Display();
     }
 }
