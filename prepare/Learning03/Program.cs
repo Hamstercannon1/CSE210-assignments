5d41fc2ac6dafd2add5974e818d8e307252e29fb
Tanner Johnson <tqjohnson17@gmail.com>
2024-05-10 20:07:31 -0600

    not sure if these are supposed to be in week 2 or week 3...

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Software engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2023;
+        job1._endYear = 2024;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Peasant";
+        job2._company = "France";
+        job2._startYear = 1413;
+        job2._endYear = 1417;
+
+        Resume myResume = new Resume();
+        myResume._name = "Allison Rose";   
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.displayJobDetails();
+
+
     }
 }
